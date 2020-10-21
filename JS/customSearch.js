a7fb6eeda96fd61bf5d24112d6fc55b5c186dc49
Chris S <65880029+JorganBonstragel@users.noreply.github.com>
2020-10-21 00:17:29 -0400

    Attempted Rewrite #367 of CustomSearch.JS

--- JS/customSearch.js
+++ JS/customSearch.js
@@ -2,22 +2,20 @@
 let userInput = document.getElementById('user_search');
 let gallery = document.getElementById('gallery');
 
-
-// User input value
-user_search.addEventListener("keyup", e => { 
-    const searchString = e.target.value.toUpperCase(); 
-    console.log([userInput])
-// for any input check the data caption (upperCase sensitive)
-for (let count = 0; count < gallery.length; count++) {
-    const userInput = gallery[count].getAttribute('data-caption').toUpperCase()
-    
-// if user includes letters or name block the image on screen
-     if (userInput.includes(searchString)) {
-        gallery[count].style.display = 'block';
-            
-// if nothing matches display no image
-    } else {
-        gallery[count].style.display = 'none';
+//search function
+function search(userInput){
+    for(let i = 0; i < gallery.length; i++){
+        userInput = userInput.toUpperCase();
+        const caption = gallery[i].getAttribute('data-caption').toUpperCase();
+        if(userInput.includes(caption)){
+            gallery[i].style.display = 'block';
+        }
+        else{
+            gallery[i].style.display = 'none';
         }
     }
-});
+}
+
+//call search function on keyup when focus is user_search
+user_search.addEventListener("keyup", search());
+
